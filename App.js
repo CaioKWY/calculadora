69e8aa430384def149fa28e84ee58961d93c6ae5
CaioKWY <caiokaway@gmail.com>
2024-03-02 20:08:50 -0300

    ajuste App componente Display

--- App.js
+++ App.js
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView, StyleSheet, Text, View
 } from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}> Calculadora</Text>
+export default class App extends Component {
 
-      <View style={styles.button}>
-        <Button label='AC' />
-        <Button label='/' />
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='*' />
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
-        <Button label='0' />
-        <Button label='.' />
-        <Button label='=' />
+  state = {
+    displayValue: "0"
+  }
+
+  render() {
+    return (
+      <View style={styles.sectionContainer}>
+
+        <Display value={this.state.displayValue} />
+
+        <View style={styles.button}>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
+        </View>
+
       </View>
-
-    </SafeAreaView>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
